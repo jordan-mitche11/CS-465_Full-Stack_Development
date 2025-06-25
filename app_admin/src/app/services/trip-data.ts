167af42b06bc7ebce452386e965f187a3f652059
jordan-mitche11 <148724875+jordan-mitche11@users.noreply.github.com>
2025-06-25 18:08:18 -0400

    Finish Angular trip form and partial backend route setup

--- app_admin/src/app/services/trip-data.ts
+++ app_admin/src/app/services/trip-data.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-
 import { Trip } from '../models/trip';
 
 @Injectable({
@@ -9,14 +8,13 @@ import { Trip } from '../models/trip';
 })
 export class TripDataService {
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
+  url = 'http://localhost:3000/api/trips;'
 
   getTrips() : Observable<Trip[]> {
-    let url = 'http://localhost:3000/api/trips';
-    
-    return this.http.get<Trip[]>(url);
+    return this.http.get<Trip[]>(this.url);
   }
-
+    
   addTrip(formData: Trip): Observable<Trip> {
     return this.http.post<Trip>(this.url, formData);
   }
